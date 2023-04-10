4732f6b7161bc006526353b136b6222d408233c2
0xlkda <lekhacduyanh@gmail.com>
2023-04-10 17:10:58 +0700

    update event-bus

--- event-bus.js
+++ event-bus.js
@@ -1,10 +1,8 @@
 class EventBus {
-  #env
   #eventTarget
 
-  constructor(description, env) {
-    this.#env = env
-    this.#eventTarget = this.#env.appendChild(this.#env.createComment(description))
+  constructor(eventTarget) {
+    this.#eventTarget = eventTarget
   }
 
   getTargetEvent() {
@@ -12,23 +10,66 @@ class EventBus {
   }
 
   on(type, listener) {
-    this.#eventTarget.addEventListener(type, listener)
+    this.getTargetEvent().addEventListener(type, listener)
     return () => this.off(type, listener)
   }
 
   once(type, listener) {
-    this.#eventTarget.addEventListener(type, listener, { once: true })
+    this.getTargetEvent().addEventListener(type, listener, { once: true })
   }
 
   off(type, listener) {
-    this.#eventTarget.removeEventListener(type, listener)
+    this.getTargetEvent().removeEventListener(type, listener)
   }
 
-  emit(type) {
-    return (detail) => this.#eventTarget.dispatchEvent(new CustomEvent(type, { detail }))
+  emit(type, detail) {
+    return this.getTargetEvent().dispatchEvent(new CustomEvent(type, { detail }))
+  }
+
+  lazyEmit(type) {
+    return (detail) => this.emit(type, detail)
   }
 }
 
-export function createEventBus(name, env = document) {
-  return new EventBus(name, env)
+
+function withLog(bus, logger = console, debugMethods = ['emit']) {
+  const busWithLog = new Proxy(bus, {
+    get(target, prop, receiver) {
+      const value = target[prop]
+
+      if (debugMethods.includes(prop)) {
+        logger.log(prop)
+      }
+
+      if (value instanceof Function) {
+        return function(...args) {
+          return value.apply(this === receiver ? target : this, args)
+        }
+      }
+
+      return value
+    }
+  })
+
+  return busWithLog
+}
+
+function createBrowserEventTarget(name) {
+  return document.appendChild(document.createComment(name))
+}
+
+export function createEventBus(name, env, options = {}) {
+  switch (env) {
+  case 'browser': {
+    const eventTarget = createBrowserEventTarget(name)
+    const bus = new EventBus(eventTarget)
+
+    return options.debug
+      ? withLog(bus, options.logger, options.debugMethods)
+      : bus
+  }
+
+  default:
+    throw new Error('not implemented')
+  }
 }
